6ded7ed4c466651d1660528e1d4440a2e02a988b
LunaHalsey <LunaHalsey@users.noreply.github.com>
2023-10-17 15:04:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// // script.js
+// const openButton = document.getElementById("openButton");
+// const popup = document.getElementById("popup");
+// const closeButton = document.getElementById("closeButton");
+
+// openButton.addEventListener("click", () => {
+//     popup.style.display = "block";
+// });
+
+// closeButton.addEventListener("click", () => {
+//     popup.style.display = "none";
+// });
+
+// script.js
+const openButton = document.getElementById("openButton");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementById("closeButton");
+
+openButton.addEventListener("click", () => {
+    popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+    popup.style.display = "none";
+});
+
+// Close the pop-up when clicking outside the pop-up
+window.addEventListener("click", (event) => {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+});
